18071f1280fe789d5f509351d4c166b4ce5b3f1f
Ege Aydin <egeaydin@gmail.com>
2017-06-21 15:35:20 -0700

    right click added

--- mousy/MousyMacApp/Mouse.swift
+++ mousy/MousyMacApp/Mouse.swift
@@ -19,22 +19,25 @@ class Mouse
             print("\(package.action), \(xyz)")
             point = xyz.getAsMousePoint(pointer: &point)
             CGWarpMouseCursorPosition(CGPoint(x: point.x, y: point.y))
-    
-       // https://stackoverflow.com/questions/2734117/simulating-mouse-input-programmatically-in-os-x/41196453#41196453
         case Action.mouseLeftClick:
             print("left click")
-            guard let downEvent = CGEvent(mouseEventSource: nil, mouseType: .leftMouseDown, mouseCursorPosition: point, mouseButton: .left) else {
-                return
-            }
-            guard let upEvent = CGEvent(mouseEventSource: nil, mouseType: .leftMouseUp, mouseCursorPosition: point, mouseButton: .left) else {
-                return
-            }
-            downEvent.post(tap: CGEventTapLocation.cghidEventTap)
-            upEvent.post(tap: CGEventTapLocation.cghidEventTap)
-            
+            self.mouseClick(downEvent: CGEventType.leftMouseDown, upEvent: CGEventType.leftMouseUp, point: point, button: CGMouseButton.left)
         case Action.mouseRightClick:
             print("right click")
+            self.mouseClick(downEvent: CGEventType.rightMouseDown, upEvent: CGEventType.rightMouseUp, point: point, button: CGMouseButton.right)        }
+    }
+    
+    // https://stackoverflow.com/questions/2734117/simulating-mouse-input-programmatically-in-os-x/41196453#41196453
+    private class func mouseClick(downEvent: CGEventType, upEvent: CGEventType, point: CGPoint, button: CGMouseButton)
+    {
+        guard let downEvent = CGEvent(mouseEventSource: nil, mouseType: downEvent, mouseCursorPosition: point, mouseButton: button) else {
+            return
         }
+        guard let upEvent = CGEvent(mouseEventSource: nil, mouseType: upEvent, mouseCursorPosition: point, mouseButton: button) else {
+            return
+        }
+        downEvent.post(tap: CGEventTapLocation.cghidEventTap)
+        upEvent.post(tap: CGEventTapLocation.cghidEventTap)
     }
     
     
